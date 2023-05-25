74c5659b910b92fedd1d159e7c852f5827d7c2f5
Muhammad Nasrul <muhammad.nasrul@pt-ssss.com>
2023-05-25 09:39:38 +0700

    oke

--- lib/gambar.dart
+++ lib/gambar.dart
@@ -465,20 +465,33 @@ class PrefetchImageDemo extends StatefulWidget {
 
 class _PrefetchImageDemoState extends State<PrefetchImageDemo> {
   final List<String> images = [
-    'https://images.unsplash.com/photo-1586882829491-b81178aa622e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
-    'https://images.unsplash.com/photo-1586871608370-4adee64d1794?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2862&q=80',
-    'https://images.unsplash.com/photo-1586901533048-0e856dff2c0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
-    'https://images.unsplash.com/photo-1586902279476-3244d8d18285?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
-    'https://images.unsplash.com/photo-1586943101559-4cdcf86a6f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1556&q=80',
-    'https://images.unsplash.com/photo-1586951144438-26d4e072b891?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
-    'https://images.unsplash.com/photo-1586953983027-d7508a64f4bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    // 'http://127.0.0.1:8080/images/1.avif',
+    // 'http://127.0.0.1:8080/images/2.avif',
+    // 'http://127.0.0.1:8080/images/3.avif',
+    // 'http://127.0.0.1:8080/images/4.avif',
+    // 'http://127.0.0.1:8080/images/5.avif',
+    // 'http://127.0.0.1:8080/images/6.avif',
+    // 'http://127.0.0.1:8080/images/7.avif',
+     'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+
   ];
 
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       images.forEach((imageUrl) {
-        precacheImage(NetworkImage(imageUrl), context);
+        // if (imageUrl.contains("http")){
+            precacheImage(NetworkImage(imageUrl), context); 
+        // // }else {
+        //   Image.asset(imageUrl);
+        // }
+        // 
+        
       });
     });
     super.initState();
@@ -497,6 +510,7 @@ class _PrefetchImageDemoState extends State<PrefetchImageDemo> {
           enlargeCenterPage: true,
         ),
         itemBuilder: (context, index, realIdx) {
+          print(images[index]);
           return Container(
             child: Center(
                 child: Image.network(images[index],
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:aplikasi_6simic1_f3/gambar.dart';
 import 'package:aplikasi_6simic1_f3/volume.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:aplikasi_6simic1_f3/luas.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -126,31 +127,231 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+  final List<String> images = [
+    // 'http://127.0.0.1:8080/images/1.avif',
+    // 'http://127.0.0.1:8080/images/2.avif',
+    // 'http://127.0.0.1:8080/images/3.avif',
+    // 'http://127.0.0.1:8080/images/4.avif',
+    // 'http://127.0.0.1:8080/images/5.avif',
+    // 'http://127.0.0.1:8080/images/6.avif',
+    // 'http://127.0.0.1:8080/images/7.avif',
+     'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+
+  ];
 
 class MyDashboard extends StatelessWidget {
   const MyDashboard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Beranda")),
-      body: Column(
-        children: [
-          const Center(
-              child: Text(
-            "Hello Word",
-            style: TextStyle(
-              fontFamily: 'DancingScript',
+ Widget menuIcon({
+    required Size deviceSize,
+    required String text,
+    required IconData icon,
+  }) {
+    return Container(
+      height: 60.0,
+      width: deviceSize.width / 5,
+      padding: const EdgeInsets.all(2.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Icon(
+              icon,
+              color: Colors.grey[100],
             ),
-          )),
-          Image.asset(
-            '../images/d.png',
-            height: 250,
-            width: 500,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.grey[100],
+            ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
+    );
+  }
+
+  Widget mainMenuIcon({
+    required Size deviceSize,
+    required String text,
+    required String src,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(6.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: CircleAvatar(
+              child: Image.asset(src),
+              foregroundColor: Colors.red,
+              backgroundColor: Colors.white,
+            ),
+            width: 80.0,
+            height: 80.0,
+            padding: EdgeInsets.all(2.0), // borde width
+            decoration: BoxDecoration(
+              color: Colors.grey[200], // border color
+              shape: BoxShape.circle,
+            ),
+          ),
+          Text(text),
+        ],
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+      Size deviceSize = MediaQuery.of(context).size;
+    double  width = MediaQuery.of(context).size.width;
+    // const halfWidth = width/2;
+    double height = MediaQuery. of(context).size.height;
+    // const sizedBox = SizedBox(
+    //             width: halfWidth,
+    //             height: 100,
+    //             child: Center(child: Text('Filled Card')),
+    //           );
+    return Scaffold(
+      appBar: AppBar(title: const Text("Beranda")),
+      body:SingleChildScrollView(child:  Column(
+        children: [
+      CarouselSlider.builder(
+        itemCount: images.length,
+        options: CarouselOptions(
+          autoPlay: true,
+          aspectRatio: 2.0,
+          enlargeCenterPage: true,
+        ),
+        itemBuilder: (context, index, realIdx) {
+          return  Container(
+            child: Center(
+                child: Image.network(images[index],
+                    fit: BoxFit.cover)),
+          );
+        },
+      ),
+      const SizedBox(
+        height: 10,
+      ),
+      Row(
+        children: [
+          Container(
+        child:const Card( child: SizedBox(
+            height: 10,
+          ),) ,
+        height: height * 0.1,
+        width: width * 0.5,
+      ),
+        Container(
+        // color: Colors.blue,
+        height: height * 0.1,
+        width: width * 0.5,
+        child: const Card(
+          child:  SizedBox(
+            height: 10,
+          ),
+      ),
+      ),
+        ],
+      ),
+      Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/listrik.png",
+              text: "PLN",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/pulsa.png",
+              text: "Pulsa",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/paket_data.png",
+              text: "Paket Data",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/pasca_bayar.png",
+              text: "Pasca Bayar",
+            ),
+            mainMenuIcon(
+                          deviceSize: deviceSize,
+                          src: "../res/images/bpjs.png",
+                          text: "BPJS",
+                        ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/tv_kabel.png",
+              text: "TV Kabel",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/asuransi.png",
+              text: "Asuransi",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/lihat_semua.png",
+              text: "Lihat Semua",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/asuransi.png",
+              text: "Asuransi",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/lihat_semua.png",
+              text: "Lihat Semua",
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/tv_kabel.png",
+              text: "TV Kabel",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/asuransi.png",
+              text: "Asuransi",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/lihat_semua.png",
+              text: "Lihat Semua",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/asuransi.png",
+              text: "Asuransi",
+            ),
+            mainMenuIcon(
+              deviceSize: deviceSize,
+              src: "../res/images/lihat_semua.png",
+              text: "Lihat Semua",
+            ),
+          ],
+        ),
+        ],
+         
+      ),
+      
+      ),floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(
           Icons.call,
