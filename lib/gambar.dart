7b2807626485518069f52ef698833f6b1896a8cd
Muhammad Nasrul <muhammad.nasrul@pt-ssss.com>
2023-05-25 10:02:38 +0700

    oke

--- lib/gambar.dart
+++ lib/gambar.dart
@@ -465,20 +465,19 @@ class PrefetchImageDemo extends StatefulWidget {
 
 class _PrefetchImageDemoState extends State<PrefetchImageDemo> {
   final List<String> images = [
-    // 'http://127.0.0.1:8080/images/1.avif',
+    'images/1.avif',
     // 'http://127.0.0.1:8080/images/2.avif',
     // 'http://127.0.0.1:8080/images/3.avif',
     // 'http://127.0.0.1:8080/images/4.avif',
     // 'http://127.0.0.1:8080/images/5.avif',
     // 'http://127.0.0.1:8080/images/6.avif',
     // 'http://127.0.0.1:8080/images/7.avif',
-     'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-
+    //    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    // 'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    // 'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    // 'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    // 'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    // 'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
   ];
 
   @override
@@ -486,12 +485,11 @@ class _PrefetchImageDemoState extends State<PrefetchImageDemo> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       images.forEach((imageUrl) {
         // if (imageUrl.contains("http")){
-            precacheImage(NetworkImage(imageUrl), context); 
+        precacheImage(NetworkImage(imageUrl), context);
         // // }else {
         //   Image.asset(imageUrl);
         // }
-        // 
-        
+        //
       });
     });
     super.initState();
@@ -513,8 +511,8 @@ class _PrefetchImageDemoState extends State<PrefetchImageDemo> {
           print(images[index]);
           return Container(
             child: Center(
-                child: Image.network(images[index],
-                    fit: BoxFit.cover, width: 1000)),
+                child:
+                    Image.asset(images[index], fit: BoxFit.cover, width: 1000)),
           );
         },
       )),
